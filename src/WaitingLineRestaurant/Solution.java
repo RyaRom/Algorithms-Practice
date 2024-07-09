b648bb3437b6c2557960ab2ff680d0835d480485
NekoNyan62 <riabikin.roman@gmail.com>
2024-07-09 17:36:25 +0300

    daily task

---
+++ src/WaitingLineRestaurant/Solution.java
@@ -0,0 +1,18 @@
+package WaitingLineRestaurant;
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().averageWaitingTime(new int[][]{{1,2},{2,5},{4,3}}));
+    }
+    public double averageWaitingTime(int[][] customers) {
+        int startTime = 0;
+        long average=0;
+         for (int[] customer : customers) {
+            startTime = Math.max(startTime, customer[0]);
+            int finishTime = startTime + customer[1];
+            average += (finishTime - customer[0]);
+            startTime = finishTime;
+        }
+        return (double) average /customers.length;
+    }
+}
