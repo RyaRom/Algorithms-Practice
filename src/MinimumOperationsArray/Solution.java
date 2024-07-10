328a446473ff484c9f171e5cef98a9e3f25f539f
NekoNyan62 <riabikin.roman@gmail.com>
2024-07-10 18:39:19 +0300

    daily task

---
+++ src/MinimumOperationsArray/Solution.java
@@ -0,0 +1,13 @@
+package MinimumOperationsArray;
+
+class Solution {
+    public int minOperations(int n) {
+        int result=0;
+        int average = n-1;
+        while (average > 0){
+            result += average;
+            average-=2;
+        }
+        return result;
+    }
+}
