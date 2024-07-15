b09f3e8b237f8193dc39992046dbded3b90f4646
NekoNyan62 <riabikin.roman@gmail.com>
2024-07-15 16:49:20 +0300

    daily task

---
+++ src/BinaryTree/TreeNode.java
@@ -0,0 +1,17 @@
+package BinaryTree;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    @Override
+    public String toString() {
+        return "TreeNode{" +
+                "val=" + val +
+                ", left=" + left +
+                ", right=" + right +
+                '}';
+    }
+}
