90ac432293118220cf9f7370dd08e176d2cd9d2d
NekoNyan62 <riabikin.roman@gmail.com>
2024-07-22 14:23:50 +0300

    update

--- src/YandexHandbookAlg/Task3_6_2.java
+++ src/YandexHandbookAlg/Task3_6_2.java
@@ -2,22 +2,34 @@ package YandexHandbookAlg;
 
 import java.util.Arrays;
 import java.util.Objects;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Task3_6_2 {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int n = s.nextInt();
-        int arr[] = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = s.nextInt();
+
+        while (true) {
+            Random random= new Random();
+            int n = random.nextInt(1, 100);
+            int[] arr = IntStream.generate(random::nextInt).limit(n).toArray();
+            int[] arr2 = Arrays.stream(arr).toArray();
+            quickSortRandom(arr2, 0, n);
+            String qs = Arrays.stream(arr2).boxed().map(Objects::toString).collect(Collectors.joining(" "));
+            Arrays.sort(arr);
+            String correct = Arrays.stream(arr).boxed().map(Objects::toString).collect(Collectors.joining(" "));
+            if (!correct.equals(qs)){
+                System.out.printf("%s\n\n is wrong      \n\n    %s\n\n is correct\n\n\n\n", qs, correct);
+                break;
+            }else {
+                System.out.printf("%s\n\n is quickSort      \n\n    %s\n\n is correct\n\n\n\n", qs, correct);
+            }
         }
-        quickSortRandom(arr, 0, n);
-        System.out.println(Arrays.stream(arr).boxed().map(Objects::toString).collect(Collectors.joining(" ")));
+
     }
     private static void quickSortRandom(int[] arr, int start, int end){
-        if (end <= start) return;
+        if (end-1 <= start) return;
         int random;
         do {
             random = (int) (Math.random() * end);
