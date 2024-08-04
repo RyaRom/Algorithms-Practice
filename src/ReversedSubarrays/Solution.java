d47b6eded12417970bd3676b325ca7c7efe109d4
NekoNyan62 <riabikin.roman@gmail.com>
2024-08-04 12:19:40 +0300

    update

--- src/ReversedSubarrays/Solution.java
+++ src/ReversedSubarrays/Solution.java
@@ -1,15 +1,13 @@
 package ReversedSubarrays;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        if (target.length!=arr.length)return false;
-        Map<Integer, Integer> targetMap = new HashMap();
-        Map<Integer, Integer> arrMap = new HashMap();
-        for (int j : target) targetMap.put(j, targetMap.get(j)==null?1:targetMap.get(j)+1);
-        for (int j : arr) arrMap.put(j, arrMap.get(j)==null?1:arrMap.get(j)+1);
-        return targetMap.equals(arrMap);
+        Arrays.sort(target);
+        Arrays.sort(arr);
+        return Arrays.equals(arr, target);
     }
 }
---
+++ src/SubarraySorting/Solution.java
@@ -0,0 +1,35 @@
+package SubarraySorting;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().rangeSum(new int[]{1,2,3,4},4,1,5));
+    }
+    private long sum(int s, int f, int[] arr){
+        long sum = 0;
+        for (int i = s; i <= f; i++) {
+            sum+=arr[i];
+        }
+        return sum;
+    }
+    private long sum(int s, int f, List<Long> arr){
+        long sum = 0;
+        for (int i = s; i <= f; i++) {
+            sum+=arr.get(i);
+        }
+        return sum;
+    }
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        List<Long> subArrays = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                subArrays.add(sum(i,j,nums));
+            }
+        }
+        Collections.sort(subArrays);
+        return (int) (sum(left-1, right-1, subArrays) %( 1000000000+7));
+    }
+}
