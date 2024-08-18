d28db4e6bc53ca85a87541ab1f292a5b3df1639e
Roman Ryabikin <riabikin.roman@gmail.com>
2024-08-18 18:55:54 +0300

    update

---
+++ src/uglyNumbers/Solution.kt
@@ -0,0 +1,20 @@
+package uglyNumbers
+
+import java.util.TreeSet
+
+class Solution {
+    fun nthUglyNumber(n: Int): Int {
+        val dp = ArrayList<Long>()
+        var toAdd = TreeSet<Long>()
+        toAdd.add(1)
+        while (!toAdd.isEmpty()){
+            val small = toAdd.first()
+            toAdd.add(small*2)
+            toAdd.add(small*3)
+            toAdd.add(small*5)
+            dp.add(toAdd.pollFirst())
+            if (dp.size == n)return dp.last().toInt()
+        }
+        return dp[n-1].toInt()
+    }
+}
