db82c2aa111e754aea13011699d0cd80f5e4db13
Roman Ryabikin <riabikin.roman@gmail.com>
2024-09-01 11:24:23 +0300

    update

---
+++ src/vectorToMatrix/Solution.kt
@@ -0,0 +1,16 @@
+package vectorToMatrix
+
+class Solution {
+    fun construct2DArray(original: IntArray, m: Int, n: Int): Array<IntArray> {
+        if (n * m != original.size) return arrayOf()
+        val matrix = Array(m) { IntArray(n) }
+        var k = 0
+        for (i in 0 until m) {
+            for (j in 0 until n) {
+                matrix[i][j] = original[k]
+                k++
+            }
+        }
+        return matrix
+    }
+}
