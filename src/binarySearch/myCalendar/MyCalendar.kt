31fd3776e22f66455a1726be2eb6f4049c019237
Roman Ryabikin <riabikin.roman@gmail.com>
2024-09-27 15:02:47 +0300

    update

--- src/binarySearch/myCalendar/MyCalendar.kt
+++ src/binarySearch/myCalendar/MyCalendar.kt
@@ -1,11 +1,13 @@
 package binarySearch.myCalendar
 
+import binarySearch.myCalendar.MyCalendar.Interval
 import java.util.*
 
 fun main() {
-    val calendar = MyCalendar()
-    println(calendar.book(47, 50))
-    println(calendar.book(33, 41))
+    val calendar = MyCalendarTwo()
+    println(calendar.book(1, 4))
+    println(calendar.book(3, 5))
+    println(calendar.book(3, 5))
 }
 
 class MyCalendar {
@@ -13,6 +15,7 @@ class MyCalendar {
         override fun compareTo(other: Interval): Int {
             return start.compareTo(other.start)
         }
+
     }
 
     private val events = TreeSet<Interval>()
@@ -39,10 +42,40 @@ class MyCalendar {
     }
 }
 
-class MyCalendarTwo() {
+class MyCalendarTwo {
+    private val events = TreeSet<Interval>()
+    private val doubleBooked = TreeSet<Interval>()
 
     fun book(start: Int, end: Int): Boolean {
+        val newEvent = Interval(start, end)
+
+        for (db in doubleBooked) {
+            if (newEvent.overlap(db)) {
+                return false
+            }
+        }
+
+        for (e in events) {
+            if (newEvent.overlap(e)) {
+                doubleBooked.add(newEvent.intersection(e))
+            }
+        }
+
+        events.add(newEvent)
         return true
     }
 
+    private class Interval(val start: Int, val end: Int) : Comparable<Interval> {
+        override fun compareTo(other: Interval): Int {
+            return start.compareTo(other.start)
+        }
+
+        fun overlap(other: Interval): Boolean {
+            return this.start < other.end && other.start < this.end
+        }
+
+        fun intersection(other: Interval): Interval {
+            return Interval(maxOf(this.start, other.start), minOf(this.end, other.end))
+        }
+    }
 }
