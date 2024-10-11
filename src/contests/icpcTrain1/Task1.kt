622567b7a4710dd55f381b53d75471926cb79b39
Roman Ryabikin <riabikin.roman@gmail.com>
2024-10-11 19:57:15 +0300

    update

---
+++ src/contests/icpcTrain1/Task1.kt
@@ -0,0 +1,33 @@
+package contests.icpcTrain1
+
+fun main() {
+    val i = readln().toInt()
+    val values = arrayOf(
+        "ITMO",
+        "SPbSU",
+        "SPbSU",
+        "ITMO",
+        "ITMO",
+        "SPbSU",
+        "ITMO",
+        "ITMO",
+        "ITMO",
+        "ITMO",
+        "ITMO",
+        "PetrSU, ITMO",
+        "SPbSU",
+        "SPbSU",
+        "ITMO",
+        "ITMO",
+        "ITMO",
+        "ITMO",
+        "SPbSU",
+        "ITMO",
+        "ITMO",
+        "ITMO",
+        "ITMO",
+        "ITMO",
+        "SPbSU"
+    )
+    println(values[i-1995])
+}
